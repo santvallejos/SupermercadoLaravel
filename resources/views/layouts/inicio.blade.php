44b8d3fa2ccdafc7a081ed0ac7d821d17052a3ba
santvallejos <vallejossantiago1412@gmail.com>
2024-04-28 20:15:45 -0300

    Creacion de carrusel de imagenes

--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -5,5 +5,18 @@
 
 {{-- Contenido principal de la pagina --}}
 @section('content')
-    <h1>Pagina de Inicio</h1>
+    <div class="carrusel">
+        <div class="mySlides">
+            <img src="{{ asset('img/carruselImg1.jpg') }}" alt="imagen-1">
+        </div>
+        <div class="mySlides">
+            <img src="{{ asset('img/carruselImg2.jpg') }}" alt="">
+        </div>
+        <div class="mySlides">
+            <img src="{{ asset('img/carruselImg3.jpg') }}" alt="">
+        </div>
+        
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
 @endsection
