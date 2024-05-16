228cac4e4d3756cbc99423982b67d17dd3e5e1c2
maximilianopaterno <maximilianopaterno83@gmail.com>
2024-05-16 15:59:44 -0300

    Cree tabla UserData y Add_softdeletes_to_user

---
+++ database/migrations/2024_05_16_184534_create_user_data_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_data', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('username');
+            $table->date('fechadenacimiento');
+            $table->string('sexo');
+            $table->string('email')->unique();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_data');
+    }
+}
