4173ab51c8bd082a897f14555d9aa9b735daf7cf
maximilianopaterno <maximilianopaterno83@gmail.com>
2024-05-17 05:10:43 -0300

    cree el modelo UserData, elimine la tabla userData ya que estaba mal creada, cree la tabla usersData y cree el controlador UserController

---
+++ app/Models/UserData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserData extends Model
+{
+    use HasFactory;
+    protected $table='users_data';
+    protected $fillable = [
+        'name',
+        'username',
+        'fechadenacimiento',
+        'sexo',
+        'email',
+        'password',
+    ];
+}
--- database/migrations/2024_05_17_075930_create_users_data_table.php
+++ database/migrations/2024_05_17_075930_create_users_data_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserDataTable extends Migration
+class CreateUsersDataTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserDataTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_data', function (Blueprint $table) {
+        Schema::create('users_data', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('name');
@@ -35,6 +35,6 @@ class CreateUserDataTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_data');
+        Schema::dropIfExists('users_data');
     }
 }
