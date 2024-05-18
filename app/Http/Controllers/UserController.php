cd5b3117b583ed33ec2cff47f48dc70c5d78e807
maximilianopaterno <maximilianopaterno83@gmail.com>
2024-05-18 07:33:30 -0300

    Agrega las dependencias en UserController a utilizar y eliminar campos innecesarios del metodo store

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Role;
 use App\Models\User;
+use App\Models\UserData;
+use App\Http\Requests\UserFormRequest;
+use App\Helpers\Notification;
+use Exception;
+use Auth;
+use Session;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -53,50 +59,24 @@ class UserController extends Controller
                                                              */                 
 
 
-            $validator = Validator::make($request->all(), [
-                'first_name'        => 'required|between:1,100',
-                'last_name'         => 'required|between:1,100',
-                'email'             => 'required|between:3,64|email',
-            ]);
-            if ($validator->fails()) {
-                return redirect()->back()->withInput();
-            }
-            
-           /*  echo "mobile --->  ".$request->mobile; */
-           $arrayRemove = array(" " , "(" ,")" , "-");
-           $mobile = str_replace($arrayRemove,"",$request->mobile);
-           /*         echo "<br> mobile --->  ".$mobile;
-           echo '<br> dni: '.$request->dni; */
-           $dni = str_replace(".","",$request->dni);
-           /*  echo '<br> dni: '.$dni;
-           dd('stop'); */
            dd('stop');
             $role = Role::where('id', $request->role)->first();
             $user = User::create([
                 'name'                  => $request->name,
                 'username'              => $request->username,
+                'fechadenacimiento'     => $request->fechadenacimiento,
+                'sexo'                  => $request->sexo,
                 'email'                 => $request->email,
-                'password'              => Hash::make($request->password),
+                'password'              => Hash::make($request->password),                
             ]);
-            if ($request->file('avatar')) {
-                $image = $request->file('avatar');
-                $type = $image->getClientOriginalExtension();
-                $img = date('Y-m-d-H-i-s') . '-id-' . $user->id . '.' . $type;
-                $image->move('image/user/', $img);
-
-                $avatar_image = 'image/user/' . $img;
-            } else {
-                $avatar_image = '/dist/img/user2-160x160.jpg';
-            }
             $userData = UserData::create([
-                'user_id'           =>  $user->id,
-                'first_name'        =>  $request->first_name,
-                'last_name'         =>  $request->last_name,
-                'dni'               =>  $dni,
-                'avatar'            =>  $avatar_image,
-                'address'           =>  $request->address,
-                'mobile'            =>  $mobile,
-                'date_of_birth'     =>  $request->date_of_birth,
+                'user_id'               =>  $user->id,
+                'name'                  => $request->name,
+                'username'              => $request->username,
+                'fechadenacimiento'     => $request->fechadenacimiento,
+                'sexo'                  => $request->sexo,
+                'email'                 => $request->email,
+                'password'              => Hash::make($request->password),                
             ]);
 
 
